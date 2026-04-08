73680e4c22efa334538086fceb081b97ff6975da
yogita-yedur <scfu125108@mitvpu.ac.in>
2026-04-08 12:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,96 @@
-function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
+body {
+  margin: 0;
+  font-family: Arial;
+  text-align: center;
+  background: #f5f5f5;
+}
+
+/* HEADER */
+header {
+  background: #222;
+  color: white;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+/* HOME */
+.home {
+  padding: 40px;
+}
+
+.profile {
+  width: 150px;
+  border-radius: 50%;
+  border: 4px solid #555;
+}
+
+.intro {
+  max-width: 500px;
+  margin: auto;
+}
+
+/* ABOUT */
+.about {
+  background: #fff;
+  padding: 30px;
+}
+
+.about-box {
+  max-width: 500px;
+  margin: auto;
+  text-align: left;
+}
+
+/* ACHIEVEMENTS */
+.achievements {
+  padding: 30px;
+}
+
+.card {
+  display: inline-block;
+  width: 250px;
+  margin: 10px;
+  padding: 10px;
+  background: white;
+  box-shadow: 0 0 10px gray;
+}
+
+.card img {
+  width: 100%;
+}
+
+/* CONTACT */
+.contact {
+  background: #fff;
+  padding: 30px;
+}
+
+.contact a {
+  text-decoration: none;
+  color: blue;
+}
+
+/* FOOTER */
+footer {
+  background: #222;
+  color: white;
+  padding: 10px;
+}
+
+/* DARK MODE */
+.dark-mode {
+  background: #121212;
+  color: white;
+}
+
+.dark-mode header,
+.dark-mode footer {
+  background: black;
+}
+
+.dark-mode .about,
+.dark-mode .contact,
+.dark-mode .card {
+  background: #1e1e1e;
 }
