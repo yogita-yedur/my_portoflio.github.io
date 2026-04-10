ccbec45017d5756b5ce0b18a14f3a10e4e729ec1
yogita-yedur <scfu125108@mitvpu.ac.in>
2026-04-10 09:09:15 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,11 +1,8 @@
-// Smooth scroll using mouse wheel
-document.querySelectorAll(".slide").forEach((slide, index, slides) => {
-  slide.addEventListener("wheel", (e) => {
-    e.preventDefault();
-    if (e.deltaY > 0 && slides[index + 1]) {
-      slides[index + 1].scrollIntoView({ behavior: "smooth" });
-    } else if (e.deltaY < 0 && slides[index - 1]) {
-      slides[index - 1].scrollIntoView({ behavior: "smooth" });
-    }
-  });
+// Smooth scroll
+document.querySelectorAll("a").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    });
 });
